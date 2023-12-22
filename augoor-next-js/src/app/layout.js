df6bfdb34133310c264212a75cddc93b1e4787b1
diego-alayon <dalayon@becomunica.com>
2023-12-22 17:10:00 +0100

    testing clases and components

--- augoor-next-js/src/app/layout.js
+++ augoor-next-js/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../app/globals.css'
+import 'primeicons/primeicons.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +11,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" height={['100%']}>
+      <body className={inter.className} height={['100%']}>{children}</body>
     </html>
   )
 }
